9a5d6a971e79b2d3778017700228f0f8e7843901
hugolind <hugolindstedt@outlook.com>
2022-01-06 16:43:50 +0100

    GUI

---
+++ src/org/mau/Main.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+import java.util.Properties;
+
+import org.mau.Controller;
+
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Controller controller = new Controller();
+    }
+}
