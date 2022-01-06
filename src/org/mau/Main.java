b0b1e9b8393e6089017c8bb6e62e6e23509b277a
hugolind <hugolindstedt@outlook.com>
2022-01-06 20:59:35 +0100

    test1

--- src/org/mau/Main.java
+++ src/org/mau/Main.java
@@ -1,3 +1,4 @@
+package org.mau;
 import java.sql.*;
 import org.mau.controller.Controller;
 
@@ -5,6 +6,6 @@ import org.mau.controller.Controller;
 public class Main {
     public static void main(String[] args) throws SQLException {
         Controller controller = new Controller();
-        WILMER
+        //asdasdasd
     }
 }
